e003bab40536dd71e1c4e9df501e1535579969b7
psbiomech <psbiomech@gmail.com>
2022-08-20 05:47:17 +1000

    minor updates

--- python/sandbox.py
+++ python/sandbox.py
@@ -24,6 +24,37 @@ with open(dbfilepath, "rb") as fid:
     forcedb = pk.load(fid)
 
 
+# %% LOAD AN OSIMKEY
+
+import pickle as pk
+import os
+
+# file path and name prefix
+fprefix = "FAILTCRT01_SDP01"
+fpath = r"C:\Users\Owner\Documents\data\FORCe\outputdatabase\FAILTCRT01\FAILTCRT01_SDP01"
+
+# OsimResultsKey
+pkfile = os.path.join(fpath, fprefix + "_osimkey.pkl")
+with open(pkfile, "rb") as fid: 
+    osimkey1 = pk.load(fid)
+    
+    
+    
+# %% LOAD A C3DKEY
+
+import pickle as pk
+import os
+
+# file path and name prefix
+fprefix = "FAILTCRT01_SDP01"
+fpath = r"C:\Users\Owner\Documents\data\FORCe\outputdatabase\FAILTCRT01\FAILTCRT01_SDP01"
+
+# OsimResultsKey
+pkfile = os.path.join(fpath, fprefix + "_c3dkey.pkl")
+with open(pkfile, "rb") as fid: 
+    c3dkey1 = pk.load(fid)
+    
+    
 
 
 # %% LOAD AN OSIMRESULTSKEY
@@ -32,13 +63,13 @@ import pickle as pk
 import os
 
 # file path and name prefix
-fprefix = "FAILTCRT05_SDP05"
+fprefix = "FAILTCRT01_SDP01"
 fpath = r"C:\Users\Owner\Documents\data\FORCe\outputdatabase\FAILTCRT05\FAILTCRT05_SDP05"
 
 # OsimResultsKey
 pkfile = os.path.join(fpath, fprefix + "_opensim_results.pkl")
 with open(pkfile, "rb") as fid: 
-    osimkey1 = pk.load(fid)
+    osimresultskey1 = pk.load(fid)
     
     
 
